2cc9fefa6e2ae2b8c4dfa5ea63882568623c9b2f
Alex Xiao <alex.xiao.26215@stu.he-ferrer.eu>
2026-09-18 15:04:19 +0200

    Modification pour indexation google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,41 @@ import type { JSX } from "react/jsx-runtime";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio 2026 | Alex Xiao",
+  metadataBase: new URL("https://xiao-web.com"),
+  title: {
+    default: "Portfolio 2026 | Alex Xiao",
+    template: "%s | Alex Xiao",
+  },
   description: "Portfolio 2026 | Alex Xiao - Web Design & Développeur Web",
   icons: {
     icon: "/favicon.svg",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Portfolio 2026 | Alex Xiao",
+    description: "Portfolio 2026 | Alex Xiao - Web Design & Développeur Web",
+    url: "https://xiao-web.com",
+    siteName: "Portfolio Alex Xiao",
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio 2026 | Alex Xiao",
+    description: "Portfolio 2026 | Alex Xiao - Web Design & Développeur Web",
   },
 };
 
